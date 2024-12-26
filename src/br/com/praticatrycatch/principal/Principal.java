48d9876973f7174a7721156747e4cdba4e84c210
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-26 19:42:55 -0300

    Alterar início do try, adicionar exceção InputMismatchException

--- src/br/com/praticatrycatch/principal/Principal.java
+++ src/br/com/praticatrycatch/principal/Principal.java
@@ -2,6 +2,7 @@ package br.com.praticatrycatch.principal;
 
 import br.com.praticatrycatch.excecao.DivisaoPorZeroException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,21 +10,25 @@ public class Principal {
 
             Scanner teclado = new Scanner(System.in);
 
+            try {
+
             System.out.println("Digite o primeiro número: ");
             int n1 = teclado.nextInt();
 
             System.out.println("Digite o segundo número: ");
             int n2 = teclado.nextInt();
 
-            try {
+            //try {
                 if (n2 <= 0) {
                     throw new DivisaoPorZeroException("Não é possível dividir por zero ou números negativos");
                 }
 
                 int resultado = n1/n2;
                 System.out.println("Resultado:" + resultado);
-            } catch(DivisaoPorZeroException e) {
+            } catch (DivisaoPorZeroException e) {
                 System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Digite apenas números");
             }
     }
 }
